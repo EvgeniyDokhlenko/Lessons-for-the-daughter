9d2f32caa9ecba6e5d5849ec5cf633ef730062ef
EvgeniyDokhlenko <evdohlenko@yandex.ru>
2018-04-11 23:08:47 +0400

    Замена

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		HelloWorld helloworld=new HelloWorld();
+		System.out.println("Нам нужен МИР!!!");
+		helloworld.invaders();
+		helloworld.horor();
+	}
+	void invaders(){
+		System.out.println("Желательно весь");
+	}	
+	void horor(){
+		System.out.println("Абсолютно весь");
+	}	
+}
